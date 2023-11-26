e9f20f2572e9a118069d1879ac7fb3b9d337140e
rutvasha <120074690+rutvasha@users.noreply.github.com>
2023-11-25 21:34:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Welcome to the Lobster Club Improv Comedy Show!");
+
+const btn = document.getElementById("button")
+console.log("Button?: ", btn)
+
+document.addEventListener('click', event => {
+    getJoke()
+});
+
+async function getJoke() {
+    const options = { 
+        method: 'GET', // specify this is a GET request, not a PUT or POST
+        headers: {
+          "Accept" : "application/json" // request the response in JSON format
+        }
+      }
+    try {
+        const response = await fetch('https://icanhazdadjoke.com/', options)
+        const responseAsJson = await response.json()
+        console.log(response.json())
+        document.getElementById("jokeText").innerHTML = responseAsJson.joke
+    }
+    catch (error) {
+        console.err(error)
+    }
+}
